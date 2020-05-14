7300bbf9bea59f06b28ef3777d46bab3d03ea2ed
jac08h <jac08h@protonmail.com>
2020-05-14 14:15:40 +0200

    wma2mp3 - more verbose output

--- wma2mp3/wma2mp3.py
+++ wma2mp3/wma2mp3.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from pydub import AudioSegment
 import os
 import argparse
@@ -24,22 +22,28 @@ def main():
     parser.add_argument('path', type=str, nargs='?', help='Path to directory with wma files.')
     args = parser.parse_args()
     path = args.path
-    # TODO: verbosity argument.
+    converted_count = errors_count = 0
 
     if path is None:
         print('No path specified. Exiting.')
         return
 
+    print('Collecting files.')
     wma_files = find_all_wmas(path)
+    print(f'{len(wma_files)} WMA files found.')
     for wma_file in wma_files:
         try:
             convert_wma_to_mp3(wma_file, wma_file.replace('wma', 'mp3'))
-            print(wma_file)
+            print(f"({converted_count+errors_count+1}/{len(wma_files)})  {wma_file}")
+            converted_count += 1
         except Exception as e:
             print('Error converting {}.'.format(wma_file))
             print(e)
+            errors_count += 1
 
         os.remove(wma_file)
+
+    print(f'Finished. {converted_count} files converted, {errors_count} errors.')
 
 
 if __name__ == '__main__':
